f9d7680684fb1b9edccb6672491c2ae9ddf4fe43
dhavalwork97-jpg <dhaval.work97@gmail.com>
2026-09-13 16:55:34 +0530

    feat(v34): integrate station assignment board

--- src/app/broadcast/[tournamentId]/control-room/operations/page.tsx
+++ src/app/broadcast/[tournamentId]/control-room/operations/page.tsx
@@ -1,8 +1,14 @@
 import OperationsClient from "./OperationsClient";
+import StationAssignmentBoard from "./StationAssignmentBoard";
 
 type Props = { params: Promise<{ tournamentId: string }> };
 
 export default async function ControlRoomOperationsPage({ params }: Props) {
   const { tournamentId } = await params;
-  return <OperationsClient tournamentId={tournamentId} />;
+  return (
+    <>
+      <StationAssignmentBoard tournamentId={tournamentId} />
+      <OperationsClient tournamentId={tournamentId} />
+    </>
+  );
 }
